5bcab7f72c2e73674b4945392578bc853bde0263
rkiang <ryanlkiang@gmail.com>
2018-06-04 15:00:23 -0500

    added styling to visualize the spacing

--- App.js
+++ App.js
@@ -18,6 +18,33 @@ const instructions = Platform.select({
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+const BoxModelDemo = () => ( 
+  <View style={styles.main}> 
+    <Text style={styles.content}>Column 1</Text> 
+    <Text style={styles.content}>Column 2</Text> 
+    <Text style={styles.content}>Column 3</Text> 
+  </View> 
+); 
+ 
+const styles = StyleSheet.create({ 
+  main: { 
+    flex: 1, 
+    paddingVertical: 20, 
+    flexDirection: 'row', 
+    flexWrap: 'wrap' 
+  }, 
+  content: { 
+    padding: 20, 
+    marginHorizontal: 5,
+    backgroundColor: '#ef4c', 
+    width: 125, 
+    height: 125, 
+    borderWidth: 1, 
+    borderColor: 'red', 
+    textAlign: 'center' 
+  } 
+}); 
+
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -33,26 +60,27 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <BoxModelDemo></BoxModelDemo>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 35,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#139942',
-    marginBottom: 5,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 35,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#139942',
+//     marginBottom: 5,
+//   },
+// });
